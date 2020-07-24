1f816afb10ad43167d02ed93d47ca29395753c25
nowsherlamsifat <62424563+nowsherlamsifat@users.noreply.github.com>
2020-07-24 22:11:49 +0600

    Create game_settings.py

---
+++ game_settings.py
@@ -0,0 +1,7 @@
+import pygame
+class Game_settings():
+    def __init__(self):
+        self.title='SPACE INVADER OBJECT ORIENTED'
+        self.width=800
+        self.height=600
+        self.bg=pygame.image.load('photos/bg.png')
